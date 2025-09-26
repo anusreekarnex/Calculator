4e86fadd300a85c18cb365d3189c5eaa4e3ae9f2
Anusree Mohan <anusree.karnex@gmail.com>
2025-09-26 11:25:50 +0530

    subtraction function added

--- Src/main.c
+++ Src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include "add.h"
 #include "mul.h"
+#include "sub.h"
 
 int main() {
-    int a = 8;
-    int b = 4;
+    int a;
+    int b;
+
+    printf("Enter num1 : ");
+    scanf("%d", &a);
+
+    printf("Enter num2 : ");
+    scanf("%d", &b);
 
     printf("Sum : %d\n", add(a,b));
+    printf("Difference : %d\n", sub(a,b));
     printf("Product : %d\n", mul(a,b));
 
     return 0;
